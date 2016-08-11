ef3d16ab1314be51690bfb66ddf412190bff479a
Jaku Baczy <jakbaczy@wp.pl>
2016-08-11 19:07:24 +0200

    hełhłęłę

---
+++ BikeTracker/Models/StatisticsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BikeTracker.Models
+{
+    public class StatisticsViewModel
+    {
+        public long KilometersPassed { get; set; }
+        public long tralalal { get; set; }
+
+    }
+}
